fefc826325566242b54dc3fc273cf7eb36d79a8f
kevinhenneigh <kevinhenneigh@hotmail.com>
2021-01-22 16:49:49 -0800

    Added IdentityHelper Class Changed Log In requirements and password requirements.

---
+++ JmesJemsSite/Models/IdentityHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.Models
+{
+    public static class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // Set password strength
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireDigit = false;
+            options.Password.RequireNonAlphanumeric = false;
+        }
+    }
+}
--- JmesJemsSite/Startup.cs
+++ JmesJemsSite/Startup.cs
@@ -1,4 +1,5 @@
 using JmesJemsSite.Data;
+using JmesJemsSite.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,8 +32,7 @@ namespace JmesJemsSite
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
