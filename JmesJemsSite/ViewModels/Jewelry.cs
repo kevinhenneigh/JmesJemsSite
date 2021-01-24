135f457870e6ae710dd245d8e46fba630c115302
Obe <o.bdondo@gmail.com>
2021-01-24 00:53:04 -0800

    Added ViewModels Folder and Created Jewelry Class

---
+++ JmesJemsSite/ViewModels/Jewelry.cs
@@ -0,0 +1,29 @@
+﻿using JmesJemsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.ViewModels
+{
+    public enum Type
+    {
+        Necklace, Bracelete
+    }
+    public class Jewelry
+    {
+        public int JewelryId { get; set; }
+
+        public Type Type { get; set; }
+
+        public string Color { get; set; }
+
+        public string Size { get; set; }
+
+        public List<Material> Materials { get; set; }
+
+        public double Price { get; set; }
+
+    }
+    
+}
