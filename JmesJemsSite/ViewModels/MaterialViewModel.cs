872cacd4ac13ccf91755a50892d4503db9ccb67a
Obe <o.bdondo@gmail.com>
2021-01-24 02:08:10 -0800

    Created MaterialViewModel class

---
+++ JmesJemsSite/ViewModels/MaterialViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.ViewModels
+{
+    public class MaterialViewModel
+    {
+        public int MaterialId { get; set; }
+
+        public string Title { get; set; }
+        public int JewelryId { get; set; }
+    }
+}
