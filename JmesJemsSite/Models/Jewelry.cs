5fb92ce632c431f6eb81c3e2a0dd4d05b697f8a1
Obe <o.bdondo@gmail.com>
2021-01-24 01:26:16 -0800

    Installed DynamicVML nugget package and changed Materials property to virtual list

--- JmesJemsSite/Models/Jewelry.cs
+++ JmesJemsSite/Models/Jewelry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DynamicVML;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace JmesJemsSite.Models
 
         public string Size { get; set; }
 
-        public List<Material> Materials { get; set; }
+        public virtual List<Material> Materials { get; set; }
 
         public double Price { get; set; }
         public string Image { get; set; }
