67283fb88d3cb3374129394327f04f0a3b961957
kevinhenneigh <kevinhenneigh@hotmail.com>
2021-02-02 19:32:39 -0800

    Found and corrected min. charcters in Register Password message

--- JmesJemsSite/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JmesJemsSite/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -54,7 +54,7 @@ namespace JmesJemsSite.Areas.Identity.Pages.Account
             public string Username { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
