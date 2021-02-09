b6652b0f8977590a5067dbd44eaae7aba82d1f62
Obe <o.bdondo@gmail.com>
2021-02-09 11:00:25 -0800

    Create ArtworkViewModel class with data annotations

---
+++ JmesJemsSite/ViewModels/ArtworkViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DynamicVML;
+using JmesJemsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.ViewModels
+{
+    public class ArtworkViewModel
+    {
+        public int ArtId { get; set; }
+        [Display(Name = "Name of artwork")]
+        public string Title { get; set; }
+        [Display(Name = "Type of Art")]
+        public string Type { get; set; }
+        [Display(Name = "Length")]
+        public double Length { get; set; }
+        [Display(Name = "Width")]
+        public double Width { get; set; }
+        [Display(Name = "Materials used")]
+        public virtual DynamicList<MaterialViewModel> Materials { get; set; } = new DynamicList<MaterialViewModel>();
+    }
+}
