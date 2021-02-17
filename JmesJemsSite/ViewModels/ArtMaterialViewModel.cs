a78b03417cb9bddbba5a25e9bc8f749d8abb27dd
Obe <o.bdondo@gmail.com>
2021-02-17 10:13:23 -0800

    Created ArtMaterial and ViewModel class

---
+++ JmesJemsSite/ViewModels/ArtMaterialViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.ViewModels
+{
+    public class ArtMaterialViewModel
+    {
+        public int ArtMaterialId { get; set; }
+        [Display(Name = "Material name")]
+        public string Title { get; set; }
+        [Display(Name = "Material type")]
+        public string Category { get; set; }
+    }
+}
