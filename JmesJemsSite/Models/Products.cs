46b47fcaa8a31c286cb211ca93a11397b959d3b0
kevinhenneigh <kevinhenneigh@hotmail.com>
2021-02-19 10:51:45 -0800

    Updated Database to include products

--- JmesJemsSite/Models/Products.cs
+++ JmesJemsSite/Models/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace JmesJemsSite.Models
 {
     public class Products
     {
+        [Key]
         public int ProductId { get; set; }
 
         public string Category { get; set; }
