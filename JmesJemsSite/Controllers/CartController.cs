b64981418403e0f9f6acc6fbb770d2de5d56f4f8
kevinhenneigh <kevinhenneigh@hotmail.com>
2021-02-28 14:02:51 -0800

    Added shopping cart icon Add to cart button and link  to navigation

---
+++ JmesJemsSite/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Add(int id) // Id of the product to add
+        {
+            // Get product from database
+
+            // Add product to cart cookie
+
+            // redirect to previous page
+
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            // Display all products in shopping cart
+            return View();
+        }
+    }
+}
