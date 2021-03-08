bf188799f78ea166fa8c7cba05ba178c336ebcf0
kevinhenneigh <kevinhenneigh@hotmail.com>
2021-03-08 14:46:44 -0800

    Cart controller and cookie helper working correctly Still need to fix redirect to action link

--- JmesJemsSite/Controllers/CartController.cs
+++ JmesJemsSite/Controllers/CartController.cs
@@ -31,47 +31,19 @@ namespace JmesJemsSite.Controllers
         {
             Products p = await ProductDb.GetProductAsync( _context, id);
 
-            const string CartCookie = "CartCookie";
-
-            // Get existing cart items
-            string existingItems = _httpContext.HttpContext.Request.Cookies[CartCookie];
-
-            
-            List<Products> cartProducts = new List<Products>();
-
-            // Are there items in the customers cart already
-            if (existingItems != null)
-            {
-                cartProducts = JsonConvert.DeserializeObject<List<Products>>(existingItems);
-            }
-
-            // Add current product to existing cart
-            cartProducts.Add(p);
-
-            // Add products list to cart cookie
-            string data = JsonConvert.SerializeObject(cartProducts);
-
-            // Holds items in cart for 1 year for existing customers
-            CookieOptions options = new CookieOptions()
-            {
-                Expires = DateTime.Now.AddYears(1),
-                Secure = true,
-                IsEssential = true
-            };
-
-            _httpContext.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+            CookieHelper.AddProductToCart(_httpContext, p);
 
             // redirect to previous page
-            return RedirectToAction("Index", "Summary");
+            return RedirectToAction("Index", "Products");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>A view of all products in the cart</returns>
         public IActionResult Summary()
         {
-            string cookieData = _httpContext.HttpContext.Request.Cookies["CartCookie"];
-
-            List<Products> cartProducts = JsonConvert.DeserializeObject<List<Products>>(cookieData);
-
-            return View(cartProducts);
+           return View(CookieHelper.GetCartProducts(_httpContext));
         }
     }
 }
---
+++ JmesJemsSite/Models/CookieHelper.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JmesJemsSite.Models
+{
+
+    public static class CookieHelper
+    {
+        const string CartCookie = "CartCookie";
+
+        /// <summary>
+        /// Returns current list of cart products
+        /// If cart is empty an empty list will be returned
+        /// </summary>
+        /// <param name="http"></param>
+        /// <returns>An empty list if cart is empty</returns>
+        public static List<Products> GetCartProducts(IHttpContextAccessor http)
+        {
+            // Get existing cart items
+            string existingItems = http.HttpContext.Request.Cookies[CartCookie];
+
+            List<Products> cartProducts = new List<Products>();
+
+            // Are there items in the customers cart already
+            if (existingItems != null)
+            {
+                cartProducts = JsonConvert.DeserializeObject<List<Products>>(existingItems);
+            }
+
+            return cartProducts;
+        }
+
+        public static void AddProductToCart(IHttpContextAccessor http, Products p)
+        {
+            List<Products> cartProducts = GetCartProducts(http);
+            cartProducts.Add(p);
+
+            string data = JsonConvert.SerializeObject(cartProducts);
+
+            // Holds items in cart for 1 year for existing customers
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+        } 
+
+        public static int GetTotalCartProducts(IHttpContextAccessor http)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
